181033c0be8df911717a8d8602fb6a9538f013a2
Muosvr <lujason2015@gmail.com>
2018-12-14 16:19:13 -0500

    user login validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Jason:708Jason@ds019472.mlab.com:19472/devconnector"
+  mongoURI: "mongodb://Jason:708Jason@ds019472.mlab.com:19472/devconnector",
+  secretOrKey: "secret"
 };
